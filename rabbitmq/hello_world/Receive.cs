636ae225079688d2ef0a9dd11386264594752e2e
Nizisi <54412715+Nizisi@users.noreply.github.com>
2022-12-17 23:51:51 -0500

    Add files via upload

---
+++ rabbitmq/hello_world/Receive.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+
+
+
+/*
+ * we open a connection and a channel, and declare the queue from which we're going to consume. 
+ * Note this matches up with the queue that Send publishes to.
+ */
+namespace rabbit
+{
+    class Receive
+    {
+        static void Main(string[] args) {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                /*
+                 * Note that we declare the queue here as well. 
+                 * Because we might start the consumer before the publisher, we want to make sure the queue exists before we try to consume messages from it.
+                 */
+                channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+
+                //EventingBasicConsumer.Received event handler: the queue will push us messages asynchronously, we provide a callback.
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+
+
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
---
+++ rabbitmq/hello_world/Send.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace rabbit
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            /*create a connection to the server:
+             * var: declare implicitly typed local variable,tells the compiler to figure out the type of the variable at compilation time
+             * The connection abstracts the socket connection, and takes care of protocol version negotiation and authentication and so on for us
+             */
+
+
+            var factory = new ConnectionFactory() { HostName = "localhost" }; //connect to a RabbitMQ node on the local machine - hence the localhost.
+                                                                              //If we wanted to connect to a node on a different machine we'd simply specify its hostname or IP address here.
+
+            // create a channel, which is where most of the API for getting things done resides.
+            //The using statement defines a scope at the end of which an object will be disposed.
+            using (var connection = factory.CreateConnection())
+            
+            using (var channel = connection.CreateModel())
+            {
+                /*
+                 *  Name
+                 *  Durable (the queue will survive a broker restart)
+                 *  Exclusive (used by only one connection and the queue will be deleted when that connection closes)
+                 *  Auto-delete (queue that has had at least one consumer is deleted when last consumer unsubscribes)
+                 *  Arguments (optional; used by plugins and broker-specific features such as message TTL, queue length limit, etc)
+                 */
+                channel.QueueDeclare(queue: "hello",
+                                  durable: false,
+                                  exclusive: false,
+                                  autoDelete: false,
+                                  arguments: null);
+
+
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+
+                
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        
+        }
+    }
+}
