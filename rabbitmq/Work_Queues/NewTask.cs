831de462d0c8b1e4316ed9d65636df4e2d27caf9
Nizisi <54412715+Nizisi@users.noreply.github.com>
2022-12-18 20:50:24 -0500

    task_queue

--- rabbitmq/Work_Queues/NewTask.cs
+++ rabbitmq/Work_Queues/NewTask.cs
@@ -10,17 +10,22 @@ class NewTask
         using (var connection = factory.CreateConnection())
         using (var channel = connection.CreateModel())
         {
-            channel.QueueDeclare(queue: "hello",
-                                 durable: false,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
+            //RabbitMQ doesn't allow you to redefine an existing queue with different parameters, so changed
+            channel.QueueDeclare(queue: "task_queue",
+                     durable: true,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
 
             var message = GetMessage(args);
             var body = Encoding.UTF8.GetBytes(message);
+            
+            //Marking messages as persistent,tells RabbitMQ to save the message to disk,
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
 
             channel.BasicPublish(exchange: "",
-                                 routingKey: "hello",
+                                 routingKey: "task_queue",
                                  basicProperties: null,
                                  body: body);
             Console.WriteLine(" [x] Sent {0}", message);
